dfb578d1661aede41643086e557c8e0fb4ca53b0
Jake Bladt <jake@jakebladt.com>
2017-01-07 22:32:55 -0500

    move path

--- nimrod.py
+++ nimrod.py
@@ -25,7 +25,7 @@ class ImageGrabber:
     self.target = target;
 
   def grab_image(self, url):
-    filename = urlparse(url.path);
+    filename = urlparse(url).path;
     print(self.target + filename)
 
 # urllib.urlretrieve("http://images2.fanpop.com/images/photos/6900000/cute-kitten-cats-6987468-500-431.jpg", "/mounted/nimrod/kitten.jpg")
