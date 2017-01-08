b22dacc18a4826edb036c7e51526fe72bc2e0a80
Jake Bladt <jake@jakebladt.com>
2017-01-07 22:08:13 -0500

    classy

--- nimrod.py
+++ nimrod.py
@@ -1,12 +1,28 @@
+import sys
+
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+class PageAssets:
+  def parse_page(url, depth):
+    self.url = url
+    self.page_title = soup.title.string
+
+    r = urlopen(url).read()
+    soup = BeautifulSoup(r)
+
+    all_anchors = soup.find_all('a')
+    self.links = all_anchors.get('href')
+
+    all_images = soup.find_all('img')
+    self.images = all_images.get('src')
+
+    return self
+
 # urllib.urlretrieve("http://images2.fanpop.com/images/photos/6900000/cute-kitten-cats-6987468-500-431.jpg", "/mounted/nimrod/kitten.jpg")
 
-r = urlopen("http://www.yahoo.com").read()
-soup = BeautifulSoup(r)
+url = sys.argv[1];
+depth = sys.argv[2];
 
-print(soup.title.string)
-all_anchors = soup.find_all("img")
-for anchor in all_anchors:
-  print(anchor.get('src'))
+pa = PageAssets().parse_page(url, depth)
+print(pa.page_title)
