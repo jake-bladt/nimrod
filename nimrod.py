c0ab0ad6c25c2eedb547f315e65d120d99e44368
Jake Bladt <jake@jakebladt.com>
2017-01-07 21:29:14 -0500

    try urlopen

--- nimrod.py
+++ nimrod.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
-import urllib
+from urllib.request import urlopen
+
 # urllib.urlretrieve("http://images2.fanpop.com/images/photos/6900000/cute-kitten-cats-6987468-500-431.jpg", "/mounted/nimrod/kitten.jpg")
 
-r = urllib.open("http://www.google.com").read()
+r = urlopen("http://www.google.com").read()
 soup = BeautifulSoup(r)
 
 print(soup.prettify()[0:1000])
--- provision.sh
+++ provision.sh
@@ -1,5 +1,3 @@
 apt-get update
 apt-get install python3-pip
-
-pip3 install --upgrade pip3
-pip3 install beautifulsoup4
+apt-get install python3-bs4
