7e113d5b4a123670987d87481d7be1d8f5dfc0c9
Jake Bladt <jake@jakebladt.com>
2017-01-07 21:09:13 -0500

    try sample soup

--- nimrod.py
+++ nimrod.py
@@ -1,2 +1,8 @@
+from bs4 import BeautifulSoup
 import urllib
-urllib.urlretrieve("http://images2.fanpop.com/images/photos/6900000/cute-kitten-cats-6987468-500-431.jpg", "/mounted/nimrod/kitten.jpg")
+# urllib.urlretrieve("http://images2.fanpop.com/images/photos/6900000/cute-kitten-cats-6987468-500-431.jpg", "/mounted/nimrod/kitten.jpg")
+
+r = urllib.open("http://www.google.com").read()
+soup = BeautifulSoup(r)
+
+print soup.prettify()[0:1000]
