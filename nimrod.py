2d4a49bb73d1e07a9fb1306e7ebda7de4c8f28ed
Jake Bladt <jake@jakebladt.com>
2017-01-07 21:34:32 -0500

    test parsing

--- nimrod.py
+++ nimrod.py
@@ -3,7 +3,8 @@ from urllib.request import urlopen
 
 # urllib.urlretrieve("http://images2.fanpop.com/images/photos/6900000/cute-kitten-cats-6987468-500-431.jpg", "/mounted/nimrod/kitten.jpg")
 
-r = urlopen("http://www.google.com").read()
+r = urlopen("http://www.yahoo.com").read()
 soup = BeautifulSoup(r)
 
-print(soup.prettify()[0:1000])
+print(soup.title.string)
+print(soup.find_all("a"))
